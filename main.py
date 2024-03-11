0a210f40b665ea9fa6894fe67167f92954a4a4df
Zilant1337 <grigorenko_p@inbox.ru>
2024-03-11 15:07:00 +0300

    Added formation of a starting table

--- main.py
+++ main.py
@@ -3,41 +3,38 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def DoTask(M):
-    global N,t,z,x
-    F = np.zeros((N, M))
-    for i in range(N):
-        for j in range(M):
-            F[i, j] = np.power(x[i], j)
-    w = np.linalg.inv(np.transpose(F) @ F) @ np.transpose(F) @ t
-    # w=np.linalg.pinv(F)@t
-    # y=[]
-    # for i in range(len(x)):
-    #     yval=0
-    #     for j in range(M):
-    #         yval+=w[j]*np.power(x[i],j)
-    #     y.append(yval)
-    y = F @ w
-    E = np.sum(np.power(t - y, 2)) / 2
-    if(M==1 or M==8 or M==100):
-        plt.plot(x,z,color="green")
-        plt.plot(x,y,color="red")
-        plt.scatter(x,t, color="blue")
-        plt.show()
-    return E
-
+alpha=[0.0001,0.00001,0.0000001]
 N=1000
 x = np.linspace(0, 1, N)
 z = 20*np.sin(2*np.pi * 3 * x) + 100*np.exp(x)
-shuffledX=np.copy(x)
-random.shuffle(shuffledX)
-shuffledZ=20*np.sin(2*np.pi*3*shuffledX)+100*np.exp(shuffledX)
 error = 10 * np.random.randn(N)
 t = z + error
-M=[1,8,100]
-err=[]
-for i in range(1,101):
-    err.append(DoTask(i))
-plt.plot(err)
-plt.show()
+
+shuffledX=np.copy(x)
+random.shuffle(shuffledX)
+trainX=np.copy(shuffledX[0:int(len(shuffledX)/2)])
+validationX=np.copy(shuffledX[int(len(shuffledX)/2):int(len(shuffledX)*3/2)])
+testX=np.copy(shuffledX[int(len(shuffledX)*3/2):int(len(shuffledX))])
+
+shuffledZ=20*np.sin(2*np.pi*3*shuffledX)+100*np.exp(shuffledX)
+shuffledT=shuffledZ+error
+
+F = np.zeros((N, 24))
+for i in range(N):
+    for j in range(8):
+        F[i, j] = np.power(x[i], j)
+for i in range(N):
+    for j in range(8,12):
+        F[i,j]= np.power(np.sin(x[i]),j%7)
+for i in range(N):
+    for j in range(12,16):
+        F[i,j]= np.power(np.cos(x[i]),j%11)
+for i in range(N):
+    for j in range(16,20):
+        F[i,j]= np.power(np.sqrt(x[i]),j%15)
+for i in range(N):
+    for j in range(20,24):
+        F[i,j]= np.power(np.exp(x[i]),j%19)
+print(F)
+
+
